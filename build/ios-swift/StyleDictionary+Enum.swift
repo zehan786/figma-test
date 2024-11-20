e72d6b3057b6fb678be6406e2faa7d8889b8258a
zehan786 <zehan@grupdev.co>
2024-11-20 14:35:23 +0530

    First commit

---
+++ build/ios-swift/StyleDictionary+Enum.swift
@@ -0,0 +1,57 @@
+
+//
+// StyleDictionary+Enum.swift
+//
+
+// Do not edit directly, this file was auto-generated.
+
+
+import UIKit
+
+public enum StyleDictionaryEnum {
+    public static let primitiveTokensLightColorBlack = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1)
+    public static let primitiveTokensLightColorBlack5 = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 0.050980392156862744)
+    public static let primitiveTokensLightColorBlack7 = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 0.07058823529411765)
+    public static let primitiveTokensLightColorBlue100 = UIColor(red: 0.945, green: 0.953, blue: 1.000, alpha: 1)
+    public static let primitiveTokensLightColorBlue200 = UIColor(red: 0.937, green: 0.949, blue: 0.996, alpha: 1)
+    public static let primitiveTokensLightColorBlue300 = UIColor(red: 0.875, green: 0.894, blue: 0.992, alpha: 1)
+    public static let primitiveTokensLightColorBlue400 = UIColor(red: 0.529, green: 0.600, blue: 0.965, alpha: 1)
+    public static let primitiveTokensLightColorBlue500 = UIColor(red: 0.369, green: 0.463, blue: 0.953, alpha: 1)
+    public static let primitiveTokensLightColorBlue600 = UIColor(red: 0.212, green: 0.329, blue: 0.941, alpha: 1)
+    public static let primitiveTokensLightColorBlue700 = UIColor(red: 0.169, green: 0.263, blue: 0.753, alpha: 1)
+    public static let primitiveTokensLightColorBlue800 = UIColor(red: 0.125, green: 0.196, blue: 0.561, alpha: 1)
+    public static let primitiveTokensLightColorBlue900 = UIColor(red: 0.102, green: 0.157, blue: 0.451, alpha: 1)
+    public static let primitiveTokensLightColorBlue950 = UIColor(red: 0.075, green: 0.118, blue: 0.341, alpha: 1)
+    public static let primitiveTokensLightColorGreen100 = UIColor(red: 0.663, green: 0.988, blue: 0.886, alpha: 1)
+    public static let primitiveTokensLightColorGreen200 = UIColor(red: 0.494, green: 0.984, blue: 0.831, alpha: 1)
+    public static let primitiveTokensLightColorGreen300 = UIColor(red: 0.325, green: 0.976, blue: 0.776, alpha: 1)
+    public static let primitiveTokensLightColorGreen400 = UIColor(red: 0.157, green: 0.973, blue: 0.718, alpha: 1)
+    public static let primitiveTokensLightColorGreen500 = UIColor(red: 0.031, green: 0.925, blue: 0.647, alpha: 1)
+    public static let primitiveTokensLightColorGreen600 = UIColor(red: 0.024, green: 0.757, blue: 0.529, alpha: 1)
+    public static let primitiveTokensLightColorGreen700 = UIColor(red: 0.020, green: 0.588, blue: 0.412, alpha: 1)
+    public static let primitiveTokensLightColorGreen800 = UIColor(red: 0.016, green: 0.467, blue: 0.325, alpha: 1)
+    public static let primitiveTokensLightColorGreen900 = UIColor(red: 0.012, green: 0.341, blue: 0.239, alpha: 1)
+    public static let primitiveTokensLightColorGreen950 = UIColor(red: 0.008, green: 0.220, blue: 0.153, alpha: 1)
+    public static let primitiveTokensLightColorGrey100 = UIColor(red: 0.945, green: 0.953, blue: 0.969, alpha: 1)
+    public static let primitiveTokensLightColorGrey200 = UIColor(red: 0.914, green: 0.922, blue: 0.941, alpha: 1)
+    public static let primitiveTokensLightColorGrey25 = UIColor(red: 0.988, green: 0.988, blue: 0.988, alpha: 1)
+    public static let primitiveTokensLightColorGrey300 = UIColor(red: 0.820, green: 0.835, blue: 0.871, alpha: 1)
+    public static let primitiveTokensLightColorGrey400 = UIColor(red: 0.624, green: 0.647, blue: 0.698, alpha: 1)
+    public static let primitiveTokensLightColorGrey50 = UIColor(red: 0.976, green: 0.980, blue: 0.988, alpha: 1)
+    public static let primitiveTokensLightColorGrey500 = UIColor(red: 0.408, green: 0.431, blue: 0.490, alpha: 1)
+    public static let primitiveTokensLightColorGrey600 = UIColor(red: 0.306, green: 0.337, blue: 0.388, alpha: 1)
+    public static let primitiveTokensLightColorGrey700 = UIColor(red: 0.227, green: 0.259, blue: 0.302, alpha: 1)
+    public static let primitiveTokensLightColorGrey800 = UIColor(red: 0.122, green: 0.141, blue: 0.180, alpha: 1)
+    public static let primitiveTokensLightColorGrey900 = UIColor(red: 0.075, green: 0.094, blue: 0.149, alpha: 1)
+    public static let primitiveTokensLightColorRed100 = UIColor(red: 0.980, green: 0.867, blue: 0.867, alpha: 1)
+    public static let primitiveTokensLightColorRed200 = UIColor(red: 0.949, green: 0.694, blue: 0.694, alpha: 1)
+    public static let primitiveTokensLightColorRed300 = UIColor(red: 0.918, green: 0.494, blue: 0.494, alpha: 1)
+    public static let primitiveTokensLightColorRed400 = UIColor(red: 0.890, green: 0.318, blue: 0.318, alpha: 1)
+    public static let primitiveTokensLightColorRed50 = UIColor(red: 0.988, green: 0.933, blue: 0.933, alpha: 1)
+    public static let primitiveTokensLightColorRed500 = UIColor(red: 0.863, green: 0.149, blue: 0.149, alpha: 1)
+    public static let primitiveTokensLightColorRed600 = UIColor(red: 0.690, green: 0.118, blue: 0.118, alpha: 1)
+    public static let primitiveTokensLightColorRed700 = UIColor(red: 0.514, green: 0.090, blue: 0.090, alpha: 1)
+    public static let primitiveTokensLightColorRed800 = UIColor(red: 0.412, green: 0.071, blue: 0.071, alpha: 1)
+    public static let primitiveTokensLightColorRed900 = UIColor(red: 0.310, green: 0.055, blue: 0.055, alpha: 1)
+    public static let primitiveTokensLightColorWhite = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
+}
